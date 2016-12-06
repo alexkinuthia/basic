64cc421feda5f9589bd11760296fbffd0ef3af5a
kinuthiaalex <kinale93@gmail.com>
2016-12-06 12:14:05 +0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -5,3 +5,4 @@ curl -fsSL https://get.docker.com/ | sh
 sudo groupadd docker
 sudo usermod -aG docker ubuntu
 echo "docker_OPTS="--dns 8.8.8.8"" >> /etc/default/docker
+docker build -f  https://github.com/alexkinuthia/basic.git -t basic:latest
